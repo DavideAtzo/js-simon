3c64f711491b4b0c96e48efd700f707c0619432b
DavideAtzo <atzo9477@gmail.com>
2023-03-06 16:08:43 +0100

    fix codice pt 1

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 'use strict';
 
-
-
-function randomNumber() {
-    const randomArray = [];
-    for (let i = 0; i < 5; i++) {
-        randomArray.push(Math.floor((Math.random() * 5) + 1));
-        console.log(randomArray)
-            return randomArray;
-      }
-
+function randomNumber(max) {
+    let randomArray = [];
+    while(randomArray.length < 5){
+        const numRandom = Math.floor(Math.random() * 5) + 1;
+        randomArray.push(numRandom);
+        console.log(numRandom);
+    }
+    return randomArray;
+    // for (let i = 0; i < 5; i++) {
+    //     randomArray.push(Math.floor((Math.random() * 5) + 1));
+    //     console.log(randomArray)
+    //         return randomArray;
+    //   }
 }
 
 randomNumber()
