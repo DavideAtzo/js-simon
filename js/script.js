f794847a74f634c7925fd44f1d1ef08dd8216bda
DavideAtzo <atzo9477@gmail.com>
2023-03-06 16:16:51 +0100

    pulizia codice

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ function randomNumber() {
     let randomArray = [];
     while(randomArray.length < 5){
         const numRandom = Math.floor(Math.random() * 200) + 1;
-        randomArray.push(numRandom);
         console.log(numRandom);
         if (!randomArray.includes(numRandom)) {
             randomArray.push(numRandom);
